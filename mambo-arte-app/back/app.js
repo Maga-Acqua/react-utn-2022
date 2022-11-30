6213f37e8ff78f7ea5cd1db91514e1617e9bbfdc
Maga-Acqua <maga.acqua74@gmail.com>
2022-11-29 21:38:14 -0300

    Conexion a db + pruebas

--- mambo-arte-app/back/app.js
+++ mambo-arte-app/back/app.js
@@ -3,10 +3,12 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+require('dotenv').config();
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var loginRouter = require('./routes/admin/login');
+var pool = require('./models/db');
 
 var app = express();
 
@@ -23,6 +25,35 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/admin/login', loginRouter);
+
+//CONSULTA DB
+pool.query("select * from empleados").then(function(resultados){
+  console.log(resultados);
+});
+//INSERT DB
+/*var obj = {
+  id_emp:25,
+  nombre :'Juan',
+  apellido: 'Moreira'
+}
+pool.query("insert into empleados set ?", [obj]).then(function(resultados){
+  console.log(resultados);
+});*/
+//UPDATE DB
+/*var id=1;
+var obj = {
+  nombre :'Mario',
+  apellido: 'Test'
+}
+pool.query("update empleados set ? where id_emp=?", [obj, id]).then(function(resultados){
+  console.log(resultados);
+});*/
+//DELETE DB
+/*var id=25;
+pool.query("delete from empleados where id_emp=?", [id]).then(function(resultados){
+  console.log(resultados);
+});*/
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
